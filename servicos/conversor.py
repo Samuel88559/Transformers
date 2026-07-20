ae91c206be3cdc41cb8f3f9acb9645c8e32a4998
Digitador <transformacaodigital@luariembalagens.com.br>
2026-07-20 09:26:38 -0300

    outro teste

--- servicos/conversor.py
+++ servicos/conversor.py
@@ -2,12 +2,22 @@
 from servicos.validar import validar_arquivo
 from servicos.extrator_pdf import processar_pdf
 from servicos.planilha import preencher_planilha
+from servicos.logger import registrar_info, registrar_erro
 
 def converter_pdf(arquivo):
+            try:
  
 # Comandos para executar as análises dos dados do PDF e o preenchimento do Excel
-            validar_arquivo(arquivo)
-            dados = processar_pdf(arquivo)
-            arquivo_excel = preencher_planilha(dados)
+                validar_arquivo(arquivo)
+                dados = processar_pdf(arquivo)
+                arquivo_excel = preencher_planilha(dados)
 
-            return arquivo_excel
+                print("vou registrar o log")
+
+                registrar_info(f"Conversão do {arquivo.filename} realizada com sucesso!")
+
+                return arquivo_excel
+            
+            except Exception as erro:
+                registrar_erro(str(erro))
+                raise
